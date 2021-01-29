fa77d9d2e53535a3a6ea0a47c5591adc6fe14b98
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-01-29 18:55:41 +0100

    Added visible option to hide/show tags

--- src/billboard.ts
+++ src/billboard.ts
@@ -5,6 +5,7 @@ export class Billboard extends UpdateObject {
     position : Vector3;
     canvas: HTMLCanvasElement;
     textElement: HTMLDivElement;
+    _visible: boolean = true;
 
     constructor (text: string, position: Vector3, context : Context) {
         super(context);
@@ -18,7 +19,6 @@ export class Billboard extends UpdateObject {
         this.textElement.style.height = 'fit-content';
         this.textElement.style.paddingLeft = '10px';
         this.textElement.style.paddingRight = '10px';
-        // TODO change fontSize based on distance to camera
         this.textElement.style.fontSize = '18px';
         this.textElement.style.backgroundColor = "rgba(255,255,255,0.5)";
         this.textElement.style.borderRadius = '10px';
@@ -36,7 +36,28 @@ export class Billboard extends UpdateObject {
         position2D.x = Math.round( (   position2D.x + 1 ) * this.canvas.width  / 2 );
         position2D.y = Math.round( ( - position2D.y + 1 ) * this.canvas.height / 2 );
     
-        this.textElement.style.top = (position2D.y - this.textElement.offsetHeight / 2) + 'px';
-        this.textElement.style.left = (position2D.x - this.textElement.offsetWidth / 2) + 'px';
+        var elemCoords = {
+            x: position2D.x - this.textElement.offsetWidth / 2,
+            y: position2D.y - this.textElement.offsetHeight / 2
+        }
+        this.textElement.style.left = elemCoords.x + 'px';
+        this.textElement.style.top = elemCoords.y + 'px';
+        // TODO change fontSize based on distance to camera
+        // TODO change zindex based on depth (minor)
+    }
+
+    set visible(val: boolean) {
+        if (this._visible !== val) {
+            this._visible = val;
+            if (val) {
+                document.body.appendChild(this.textElement);
+            } else {
+                document.body.removeChild(this.textElement);
+            }
+        }
+    }
+
+    get visible() {
+        return this._visible;
     }
 }
