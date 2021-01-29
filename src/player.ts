ec51d0adc45dddb079433484bc5ae87e47bc6e36
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-01-29 19:43:32 +0100

    Load asset at start and copy when instanced
    Instead of loading the asset every time Player is instanced

--- src/player.ts
+++ src/player.ts
@@ -1,9 +1,12 @@
 import { AnimationMixer, Vector3 } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Billboard } from './billboard';
 import { UpdateObject, Context } from './updateObject';
+import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
 
 export class Player extends UpdateObject {
+    static gltf : GLTF;
+
     name : string;
     model : any;
     mixer : AnimationMixer;
@@ -13,32 +16,17 @@ export class Player extends UpdateObject {
 
     constructor ( name : string, context : Context, onLoad?: () => void) {
         super(context);
-        const loader = new GLTFLoader();
         this.name = name;
 
         this.nametag = new Billboard(name, new Vector3(0, 0, 0), context);
 
+        this.model = SkeletonUtils.clone(Player.gltf.scene);
+        this.model.scale.set( 2, 2, 2);
+        this.mixer = new AnimationMixer( this.model );
+        var action = this.mixer.clipAction(Player.gltf.animations[0]);
+        action.play();
 
-        loader.load('assets/knight.glb', ( gltf ) => {
-            this.model = gltf.scene;
-            if (this._position) { 
-                this.model.position.set(this._position.x, this._position.y, this._position.z);
-            }
-            if (this._rotation) {
-                this.model.rotation.set(this._rotation.x, this._rotation.y, this._rotation.z);
-            }
-            this.model.scale.set( 2, 2, 2);
-            this.mixer = new AnimationMixer( this.model );
-            var action = this.mixer.clipAction(gltf.animations[0]);
-            action.play();
-
-            context.scene.add( this.model );
-            if (onLoad) onLoad();
-        }, undefined, ( error ) => {
-            console.error(error);
-        });
-
-
+        context.scene.add( this.model );
     }
 
     set rotation(val: Vector3) {
@@ -68,3 +56,23 @@ export class Player extends UpdateObject {
         if (this._position) {  this.nametag.position.copy(this._position).y += 2.5};
     }
 }
+
+const asset_url = 'assets/knight.glb'
+
+const loader = new GLTFLoader();
+loader.load(asset_url, ( gltf ) => {
+    Player.gltf = gltf;
+}, ( event ) => {
+    console.log(`Loading ${asset_url} - ${Math.floor(event.loaded / event.total * 100)}%`);
+}, ( reason ) => {
+    console.error(`Loading ${asset_url} failed! ${reason}`);
+});
+/*
+loader.loadAsync(asset_url, ( event ) => {
+    console.log(`Loading ${asset_url} - ${Math.floor(event.loaded / event.total * 100)}%`);
+}).then( ( gltf ) => {
+    Player.gltf = gltf;
+}).catch( (reason) => {
+    console.error(`Loading ${asset_url} failed! ${reason}`);
+});
+*/
