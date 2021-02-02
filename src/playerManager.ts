b6ca9cc5ba86a4d1b43b58e5c8a298a09811cd7f
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-02-02 23:17:49 +0100

    Add player scaling

--- src/playerManager.ts
+++ src/playerManager.ts
@@ -5,7 +5,7 @@ import { Context } from "./updateObject";
 export class PlayerManager {
     players : Player[] = [];
 
-    circle_radius_min = 7;      // min radius of player circle
+    circle_radius = 8;          // min radius of player circle
     circle_angle_max = Math.PI; // max angle between first and last player
     arc_dist = 2;               // arc distance between adjacent players
 
@@ -13,14 +13,17 @@ export class PlayerManager {
     }
 
     updatePlayerPositions() {
-        var circle_radius = this.circle_radius_min;
+        var dist = this.arc_dist;
+        var scale = Player.model_scale;
+
         // Increase circle radius if there are too many players
-        if (this.arc_dist * this.players.length > this.circle_radius_min * this.circle_angle_max) {
-            circle_radius  = this.arc_dist * this.players.length / this.circle_angle_max;
+        if (this.arc_dist * this.players.length > this.circle_radius * this.circle_angle_max) {
+            dist = this.circle_radius * this.circle_angle_max / this.players.length;
+            scale *= (dist / this.arc_dist);
         }
 
         // Angle between players based on arc distance and circle radius
-        var theta = this.arc_dist / circle_radius;
+        var theta = dist / this.circle_radius;
 
         // Get starting angle based on num players
         var angle = Math.PI/2 - (theta/2 * (this.players.length-1));
@@ -28,17 +31,14 @@ export class PlayerManager {
         this.players.forEach(player => {
             // Set player position on circle & rotate to face center
             player.rotation = new Vector3(0, -angle - Math.PI/2, 0) ;
-            player.position = new Vector3(Math.cos(angle), 0, Math.sin(angle)).multiplyScalar(circle_radius);
+            player.position = new Vector3(Math.cos(angle), 0, Math.sin(angle)).multiplyScalar(this.circle_radius);
+            player.scale = scale;
             angle += theta;
         });
         
     }
 
     addPlayer(name: string, context : Context) {
-        // Update player positions only after model is loaded in
-        //this.players.push(new Player(name, context, this.updatePlayerPositions.bind(this)));
-        
-        // Update player positions immediatly (i.e. right as player joins)
         this.players.push(new Player(name, context));
         this.updatePlayerPositions();
     }
